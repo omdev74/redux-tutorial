b4c2568769e4ab8be5e76efc3433d83b5fdd85c7
Om Dev <omdev74@gmail.com>
2021-10-10 12:24:15 +0530

    0.14.12. Passing and Retrieving Data with Action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,28 @@ const initialState = {
     counter:10
 }
 const reducer = (state = initialState,action)=>{
-    return state;
+    switch (action.type) {
+        case "INC_COUNTER":
+            return{
+                counter: state.counter + 1
+            }
+            
+        case "DEC_COUNTER":
+            return{
+                counter: state.counter -1
+            }
+
+        case "ADD_COUNTER":
+            return{
+                counter: state.counter + action.value
+            }
+        
+        case "SUB_COUNTER":
+            return{
+                counter: state.counter - action.value
+            }
+        default: return state;
+    }
+    
 }
 export default reducer
